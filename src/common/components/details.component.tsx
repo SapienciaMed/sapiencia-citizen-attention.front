8c860a849b5e56a49afc477825ef29e92d2445c6
Juan Pablo Madariaga <juanpablomadariagacardona@gmail.com>
2023-09-21 17:57:08 -0500

    [consultar pqrsdf]

--- src/common/components/details.component.tsx
+++ src/common/components/details.component.tsx
@@ -10,7 +10,7 @@ interface IDetailsProps {
 function DetailsComponent({ rows }: IDetailsProps): React.JSX.Element {
     return (
         <table className="details-table">
-            {rows.map((row) => {
+            {rows?.map((row) => {
                 return (
                     <tr key={row.title}>
                         <th className="th-title">{row.title}</th>
