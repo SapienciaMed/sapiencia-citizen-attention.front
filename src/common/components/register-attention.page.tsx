426ce6ee637619c8a007ce533c8a760681c133ac
Jehan Veracierta <desarrollador4@i4digital.com>
2023-11-14 13:27:40 -0300

    add first card

--- src/common/components/register-attention.page.tsx
+++ src/common/components/register-attention.page.tsx
@@ -33,17 +33,28 @@ const RegisterAttention = () => {
       <div className="p-4 md:p-6 max-w-[1200px] mx-auto" ref={parentForm}>
 
         <span className="text-3xl block md:hidden pb-5">Registro de atención</span>
-        <div className="p-card rounded-2xl md:rounded-4xl shadow-none border border-[#D9D9D9]">
-          <div className="p-card-body !py-6 md:!py-8 !px-6">
-            <div className="p-card-title flex justify-end md:justify-between">
-              <span className="text-3xl md:block hidden">Registro de atención</span>
-              <div className="my-auto text-base flex items-center gap-x-2">
-                <span>Registro diario de atenciones</span>
-                <label htmlFor=""></label>
-                <input type="text" />
+        <div className="p-card p-4 rounded-2xl md:rounded-4xl shadow-none border border-[#D9D9D9]">
+          <div className="p-card rounded-2xl md:rounded-4xl shadow-none border border-[#D9D9D9]">
+            <div className="p-card-body !py-6 md:!py-8 !px-6">
+              <div className="p-card-title m-auto flex justify-end md:justify-between">
+                <span className="text-3xl md:block hidden">Registro de atención</span>
+                <div className="my-auto text-base flex flex-column items-center gap-x-2">
+                  <span>Registro diario de atenciones</span>
+                  <label htmlFor="">Numero</label>
+                  <input type="text" />
+                </div>
               </div>
             </div>
           </div>
+
+          <div className="p-card rounded-2xl md:rounded-4xl shadow-none border border-[#D9D9D9]">
+            <div className="p-card-body !py-6 md:!py-8 !px-6">
+              <div className="p-card-title m-auto flex justify-end md:justify-between">
+                <p className="">Registro de atención</p>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     )
