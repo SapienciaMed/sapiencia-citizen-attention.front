4690c439e34af624459df9bc7f14040f3c105328
Jehan Veracierta <desarrollador4@i4digital.com>
2023-11-23 13:09:12 -0300

    add home

--- src/features/portal-auth/portal-auth.routes.tsx
+++ src/features/portal-auth/portal-auth.routes.tsx
@@ -2,13 +2,24 @@ import React, { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import RecoveryPassword from "../../common/components/authPage/recovery-password.page"
 
-
 function PortalAuthRoutes() {
   const PortalAuthPage = lazy(() => import("./pages/portal-login.page"));
   const Login = lazy(() => import("../../common/components/authPage/login.page"))
+  const ChangePasswordRecovery = lazy(() => import("../../common/components/authPage/change-password.page"))
+  const Register_pqrsdf = lazy(() => import("../../common/components/register_pqrsdf.page"));
 
   return (
     <Routes>
+      <Route
+        path={""}
+        element={
+          <Register_pqrsdf isPerson={true} />
+          // <PrivateRoute
+          //   element={<PortalAuthPage />}
+          //   allowedAction={"INDICADOR_ACCION_SEGURIDAD"}
+          // />
+        }
+      />
       <Route
         path={"/ingreso"}
         element={
@@ -23,6 +34,16 @@ function PortalAuthRoutes() {
         path={"/recuperar-clave"}
         element={
           <RecoveryPassword />
+          // <PrivateRoute
+          //   element={<PortalAuthPage />}
+          //   allowedAction={"INDICADOR_ACCION_SEGURIDAD"}
+          // />
+        }
+      />
+            <Route
+        path={"/cambiar-clave"}
+        element={
+          <ChangePasswordRecovery />
           // <PrivateRoute
           //   element={<PortalAuthPage />}
           //   allowedAction={"INDICADOR_ACCION_SEGURIDAD"}
