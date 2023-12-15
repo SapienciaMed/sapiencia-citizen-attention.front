5f801052acd03a10458b037159f5344bc77ca242
Daniel Flores <dflores@i4digital.com>
2023-12-15 17:08:59 -0500

    ajuste

--- src/features/portal-auth/hooks/applications-container.hook.ts
+++ src/features/portal-auth/hooks/applications-container.hook.ts
@@ -2,15 +2,17 @@ import { useMemo } from "react";
 import { IMenuAccess } from "../../../common/interfaces/menuaccess.interface";
 
 export function useApplicationsData() {
-  const applications = useMemo((): IMenuAccess[] =>
-    [
+  const applications = useMemo(
+    (): IMenuAccess[] => [
       {
         id: 1,
         name: "Presentar PQRSDF",
         order: 1,
-        url: ""
+        url: `/portal/layout/registrar-pqrsdf/$$identification`,
       },
-    ], []);
+    ],
+    []
+  );
 
   return { applications };
 }
